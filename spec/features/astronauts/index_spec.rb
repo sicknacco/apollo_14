afe0c954806b5b8fdad72dc3f58b5d6625bbb60a
Nick Sacco <nicksacco10@gmail.com>
2023-05-07 11:55:19 -0700

    us3 wip

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -24,8 +24,32 @@ RSpec.describe "Astronauts Index Page", type: :feature do
     
     it 'can show the average age of all astronauts' do
       visit '/astronauts'
-save_and_open_page
+      
       expect(page).to have_content("Average Age: 48")
+    end
+    
+    it 'shows missions for each astronaut in alphabetical order' do
+      visit '/astronauts'
+save_and_open_page
+      within "#astronauts_#{@tim.id}" do
+        expect(page).to have_content(@mission1.title)
+        expect(page).to have_content(@mission2.title)
+        
+        expect(@mission1.title).to appear_before(@mission2.title)
+        expect(@mission2.title).to_not appear_before(@mission1.title)
+      end
+      
+      within "#astronauts_#{@al.id}" do
+        expect(page).to have_content(@mission1.title)
+        expect(page).to have_content(@mission2.title)
+        expect(page).to have_content(@mission3.title)
+        expect(page).to have_content(@mission4.title)
+
+        expect(@mission1.title).to appear_before(@mission2.title)
+        expect(@mission2.title).to appear_before(@mission3.title)
+        expect(@mission3.title).to appear_before(@mission4.title)
+        expect(@mission4.title).to_not appear_before(@mission2.title)
+      end
     end
   end
 end
